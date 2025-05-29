9fca43155f8273e3fad08beebe08881bda0ef3ba
Sterling <sterling1@hotmail.ca>
2025-05-29 01:38:59 -0400

    Initial commit

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+apiKey: "YOUR-API-KEY-HERE",
+authDomain: "YOUR-AUTH-DOMAIN-HERE",
+projectId: "YOUR-PROJECT-ID-HERE",
+storageBucket: "YOUR-STORAGE-BUCKET-HERE",
+messagingSenderId: "YOUR-MESSAGING-SENDER-ID-HERE",
+appId: "YOUR-APP-ID-HERE"
+};
