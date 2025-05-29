a2b9f88c31946256835d95aa15520909183b9887
Sterling <sterling1@hotmail.ca>
2025-05-29 03:07:22 -0400

    Updated gitignore

--- firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-apiKey: "YOUR-API-KEY-HERE",
-authDomain: "YOUR-AUTH-DOMAIN-HERE",
-projectId: "YOUR-PROJECT-ID-HERE",
-storageBucket: "YOUR-STORAGE-BUCKET-HERE",
-messagingSenderId: "YOUR-MESSAGING-SENDER-ID-HERE",
-appId: "YOUR-APP-ID-HERE"
-};
